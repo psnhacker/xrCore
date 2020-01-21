f838b21a077ba2aa980ad96cfa6a1ede3d46d352
psnhacker <59035488+psnhacker@users.noreply.github.com>
2020-01-21 17:56:12 +0300

    Fix bugs

--- LOD_Settings.cs
+++ LOD_Settings.cs
@@ -3,19 +3,25 @@ using UnityEngine;
 
 public class LOD_Settings : EditorWindow
 {
+    static private float culled_distance;
+    static private float lod1_distance;
+
     [MenuItem("X-Ray Plugin/LOD System/Open LOD Settings")]
     public static void ShowWindow()
     {
         GetWindow<LOD_Settings>(false, "LOD Settings", true);
+        culled_distance = 0.06f;
+        lod1_distance = 0.4f;
     }
 
     void OnGUI()
     {
+
         EditorGUILayout.LabelField("Settings");
-        float culled_distance = 0.06f;
-        EditorGUILayout.Slider("Culled distance", culled_distance, 0f, 1f);
-        float lod1_distance = 0.4f;
-        EditorGUILayout.Slider("Lod1 Distance", lod1_distance, 0f, 1f);
+        EditorGUILayout.BeginVertical("box");
+        culled_distance = EditorGUILayout.Slider("Culled distance", culled_distance, 0f, 1f);
+        lod1_distance = EditorGUILayout.Slider("Lod1 Distance", lod1_distance, 0f, 1f);
+        EditorGUILayout.EndVertical();
         EditorGUILayout.LabelField("Controls");
         if (GUILayout.Button("Start"))
         {
