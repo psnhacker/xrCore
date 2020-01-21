e8fa58d985aab223e55431e395a9f1dcfa197127
psnhacker <59035488+psnhacker@users.noreply.github.com>
2020-01-21 17:33:16 +0300

    Update 2

--- LOD_Settings.cs
+++ LOD_Settings.cs
@@ -15,22 +15,16 @@ public class LOD_Settings : EditorWindow
         float culled_distance = EditorGUILayout.Slider("Culled distance", 0.06f, 0f, 1f);
         float lod1_distance = EditorGUILayout.Slider("Lod1 Distance", 0.4f, 0f, 1f);
         EditorGUILayout.LabelField("Controls");
-        if (GUILayout.Button("Set Value"))
+        if (GUILayout.Button("Start"))
         {
             SettingLOD.culled_distance = culled_distance;
             SettingLOD.lod1_distance = lod1_distance;
-            Debug.Log("Pressed button Set Value");
-        }
-        if (GUILayout.Button("Start"))
-        {
             CustomUtilites.LOD_Generator();
-            Debug.Log("Generation lod");
         }
         EditorGUILayout.LabelField("Create Groupe");
         if (GUILayout.Button("Split LODs"))
         {
             CustomUtilites.GetSelectedUVs();
-            Debug.Log("Pressed Split lod");
         }
     }
 }
@@ -39,5 +33,4 @@ public static class SettingLOD
 {
     public static float culled_distance;
     public static float lod1_distance;
-
 }
