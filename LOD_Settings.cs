a8b678c05e0927bcbe3292db9b772ae621af7beb
psnhacker <59035488+psnhacker@users.noreply.github.com>
2020-01-21 17:15:19 +0300

    Add class LOD_Settings

---
+++ LOD_Settings.cs
@@ -0,0 +1,35 @@
+using UnityEditor;
+using UnityEngine;
+
+public class LOD_Settings : EditorWindow
+{
+    [MenuItem("X-Ray Plugin/LOD System/Open LOD Settings")]
+    public static void ShowWindow()
+    {
+        GetWindow<LOD_Settings>(false, "LOD Settings", true);
+    }
+
+    void OnGUI()
+    {
+        float culled_distance = EditorGUILayout.Slider("Culled distance", 0.06f, 0f, 1f);
+        float lod1_distance = EditorGUILayout.Slider("Lod1 Distance", 0.4f, 0f, 1f);
+        if (GUILayout.Button("Set Value"))
+        {
+            SettingLOD.culled_distance = culled_distance;
+            SettingLOD.lod1_distance = lod1_distance;
+            Debug.Log("Pressed button Set Value");
+        }
+        if (GUILayout.Button("Start"))
+        {
+            CustomUtilites.LOD_Generator();
+            Debug.Log("Generation lod");
+        }
+    }
+}
+
+public static class SettingLOD
+{
+    public static float culled_distance;
+    public static float lod1_distance;
+
+}
